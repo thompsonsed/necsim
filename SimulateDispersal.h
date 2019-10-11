f5da24f2898db725cf66caa0a9266ee962f9b37d
thompsonsed <thompsonsed@gmail.com>
2019-10-11 15:58:30 +0100

    Fixed duplicate template error in SimulateDispersal

--- SimulateDispersal.h
+++ SimulateDispersal.h
@@ -209,7 +209,7 @@ namespace necsim
          * @param dispersal_coordinator Reference to the dispersal corrdinator to use
          * @param generation Reference to the generation variable used byt the dispersal coordinator
          */
-        template <bool chooseRandomCells=false>
+        template <bool chooseRandomCells>
         void runDistanceLoop(const unsigned long bidx,
                              const unsigned long eidx,
                              const unsigned long num_repeats,

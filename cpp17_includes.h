6161157ebd5255ffcf05e6204b234c2e2772f6e4
Sam Thompson <thompsonsed@gmail.com>
2020-05-11 01:07:31 +0100

    Updated includes for macOS to use ghc instead of default filesystem

--- cpp17_includes.h
+++ cpp17_includes.h
@@ -42,7 +42,7 @@ namespace fs = boost::filesystem;
 //#define ERROR_PRIVILEGE_NOT_HELD 1314L
 #endif
 
-#include <ghc/filesystem.hpp>
+#include "ghc/filesystem.hpp"
 
 namespace fs = ghc::filesystem;
 
@@ -50,9 +50,16 @@ namespace fs = ghc::filesystem;
 
 #else // Support for C++ 17
 
+#ifdef __APPLE__ // macOS has problems with filesystem
+
+#include "ghc/filesystem.hpp"
+
+namespace fs = ghc::filesystem;
+#else
 #include <filesystem>
 
 namespace fs = std::filesystem;
 
+#endif // __APPLE__
 #endif // __cplusplus < 201703L
 #endif //NECSIM_CPP17_INCLUDES_H

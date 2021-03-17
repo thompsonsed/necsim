238e50f099c51d538e9ab3dea51361bea30d4860
Sam Thompson <thompsonsed@gmail.com>
2021-03-17 09:13:42 +0000

    Fixed generic tree

--- GenericTree.h
+++ GenericTree.h
@@ -27,27 +27,27 @@ namespace necsim
 //
         void wipeSimulationVariables()
         {
-            tree.wipeSimulationVariables();
+            tree->wipeSimulationVariables();
         }
 
         void importSimulationVariables(std::string config_file)
         {
-            tree.importSimulationVariables(config_file);
+            tree->importSimulationVariables(config_file);
         }
 
         void importSimulationVariablesFromString(std::string config_string)
         {
-            tree.importSimulationVariablesFromString(config_string);
+            tree->importSimulationVariablesFromString(config_string);
         }
 
         void setup()
         {
-            tree.setup();
+            tree->setup();
         }
 
         bool runSimulation()
         {
-            return tree.runSimulation();
+            return tree->runSimulation();
         }
 
         void setResumeParameters(std::string pause_directory_str,
@@ -56,32 +56,32 @@ namespace necsim
                                  long task,
                                  long max_time)
         {
-            tree.setResumeParameters(pause_directory_str, out_directory_str, seed, task, max_time);
+            tree->setResumeParameters(pause_directory_str, out_directory_str, seed, task, max_time);
         }
 
         void checkSims(std::string pause_directory, long seed, long task)
         {
-            tree.checkSims(pause_directory, seed, task);
+            tree->checkSims(pause_directory, seed, task);
         }
 
         bool hasPaused()
         {
-            return tree.hasPaused();
+            return tree->hasPaused();
         }
 
         void addGillespie(const double &g_threshold)
         {
-            tree.addGillespie(g_threshold);
+            tree->addGillespie(g_threshold);
         }
 
         void addSpeciationRates(std::vector<long double> spec_rates_long)
         {
-            tree.addSpeciationRates(spec_rates_long);
+            tree->addSpeciationRates(spec_rates_long);
         }
 
         void applyMultipleRates()
         {
-            tree.applyMultipleRates();
+            tree->applyMultipleRates();
         }
     };
 }

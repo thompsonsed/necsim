6edc5d141cf241e27ed0f84d7d3d9af5e46c081f
Sam Thompson <thompsonsed@gmail.com>
2021-03-17 11:59:09 +0000

    Added generic community wrapper

---
+++ GenericCommunity.h
@@ -0,0 +1,69 @@
+//
+// Created by sam on 17/03/2021.
+//
+
+#ifndef NECSIM_GENERICCOMMUNITY_H
+#define NECSIM_GENERICCOMMUNITY_H
+
+#include <memory>
+#include <string>
+#include "SpecSimParameters.h"
+
+namespace necsim
+{
+
+    template<class T>
+    class GenericCommunity
+    {
+    private:
+        std::shared_ptr<T> community{nullptr};
+    public:
+
+        GenericCommunity<T>() : community(std::make_shared<T>())
+        { }
+
+        ~GenericCommunity() = default;
+
+        GenericCommunity(GenericCommunity &&other) noexcept : GenericCommunity()
+        {
+            *this = std::move(other);
+        }
+
+        GenericCommunity(const GenericCommunity &other) : GenericCommunity()
+        {
+            *this = other;
+        };
+
+        GenericCommunity &operator=(GenericCommunity other) noexcept
+        {
+            other.swap(*this);
+            return *this;
+        }
+
+        void swap(GenericCommunity &other) noexcept
+        {
+            if(this != &other)
+            {
+                std::swap(community, other.tree);
+
+            }
+        }
+
+        void applyNoOutput(std::shared_ptr<SpecSimParameters> sp)
+        {
+            community->applyNoOutput(sp);
+        }
+
+        void output()
+        {
+            community->output();
+        }
+
+        void speciateRemainingLineages(const string &filename)
+        {
+            community->speciateRemainingLineages(filename);
+        }
+
+    };
+}
+#endif //NECSIM_GENERICCOMMUNITY_H
